c27181c94dc48d9a1f70e1c4690aef5a4cdc2c62
Matthias <42836408+AdmiralCanelli@users.noreply.github.com>
2018-11-27 13:37:26 +0100

    Update routes Dashboard.js

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -10,9 +10,16 @@ var con = mysql.createConnection({
   });
 
   /* GET dashboard page */
-router.get('/', (req, res, next) => {
+router.get('/', isAuthenticated, (req, res, next) => {
     res.render('dashboard')
 })
 
+// Authentication 
+function isAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) {
+      return next();
+  }
+  res.redirect('/login')
+}
 
 module.exports = router;
