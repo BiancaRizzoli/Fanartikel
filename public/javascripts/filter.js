7952a73e6b394e18d25f5fabbbb5bf55385f1079
Matthias <42836408+AdmiralCanelli@users.noreply.github.com>
2019-01-28 09:03:39 +0100

    improved the filter.js code

--- public/javascripts/filter.js
+++ public/javascripts/filter.js
@@ -1,319 +1,45 @@
-$(document).ready(() => {
-    $('#btn-Bekleidung').click(() => {
-        var data = [1]
-        $.ajax({
-            url: '/category',
-            type: 'POST',
-            contentType: 'application/json',
-            data: JSON.stringify(data),
-            dataType: 'json',
-            success: (res) => {
-                for (var i = 0; i < res[0].length; i++) {
-                    document.getElementById(res[0][i]).style.display = 'block'
-                }
-                for (var i = 0; i < res[1].length; i++) {
-                    document.getElementById(res[1][i]).style.display = 'none'
-                }
-            },
-            error: () => {
-                alert('Error')
+var category = (data) => {
+    var fuckingArray = []
+    fuckingArray.push(data)
+    $.ajax({
+        url: '/category',
+        type: 'POST',
+        contentType: 'application/json',
+        data: JSON.stringify(fuckingArray),
+        dataType: 'json',
+        success: (res) => {
+            for (var i = 0; i < res[0].length; i++) {
+                document.getElementById(res[0][i]).style.display = 'block'
             }
-        })
-    })
-    $('#btn-Schmuck').click(() => {
-        var data = [2]
-        $.ajax({
-            url: '/category',
-            type: 'POST',
-            contentType: 'application/json',
-            data: JSON.stringify(data),
-            dataType: 'json',
-            success: (res) => {
-                for (var i = 0; i < res[0].length; i++) {
-                    document.getElementById(res[0][i]).style.display = 'block'
-                }
-                for (var i = 0; i < res[1].length; i++) {
-                    document.getElementById(res[1][i]).style.display = 'none'
-                }
-            },
-            error: () => {
-                alert('Error')
+            for (var i = 0; i < res[1].length; i++) {
+                document.getElementById(res[1][i]).style.display = 'none'
             }
-        })
+        },
+        error: () => {
+            console.log('Error')
+        }
     })
-    $('#btn-Repliken').click(() => {
-        var data = [3]
-        $.ajax({
-            url: '/category',
-            type: 'POST',
-            contentType: 'application/json',
-            data: JSON.stringify(data),
-            dataType: 'json',
-            success: (res) => {
-                for (var i = 0; i < res[0].length; i++) {
-                    document.getElementById(res[0][i]).style.display = 'block'
-                }
-                for (var i = 0; i < res[1].length; i++) {
-                    document.getElementById(res[1][i]).style.display = 'none'
-                }
-            },
-            error: () => {
-                alert('Error')
-            }
-        })
-    })
-    $('#btn-Schuhe').click(() => {
-        var data = [4]
-        $.ajax({
-            url: '/category',
-            type: 'POST',
-            contentType: 'application/json',
-            data: JSON.stringify(data),
-            dataType: 'json',
-            success: (res) => {
-                for (var i = 0; i < res[0].length; i++) {
-                    document.getElementById(res[0][i]).style.display = 'block'
-                }
-                for (var i = 0; i < res[1].length; i++) {
-                    document.getElementById(res[1][i]).style.display = 'none'
-                }
-            },
-            error: () => {
-                alert('Error')
-            }
-        })
-    })
-    $('#btn-Zubehör').click(() => {
-        var data = [5]
-        $.ajax({
-            url: '/category',
-            type: 'POST',
-            contentType: 'application/json',
-            data: JSON.stringify(data),
-            dataType: 'json',
-            success: (res) => {
-                for (var i = 0; i < res[0].length; i++) {
-                    document.getElementById(res[0][i]).style.display = 'block'
-                }
-                for (var i = 0; i < res[1].length; i++) {
-                    document.getElementById(res[1][i]).style.display = 'none'
-                }
-            },
-            error: () => {
-                alert('Error')
-            }
-        })
-    })
-    $('#btn-Zauberstäbe').click(() => {
-        var data = [6]
-        $.ajax({
-            url: '/category',
-            type: 'POST',
-            contentType: 'application/json',
-            data: JSON.stringify(data),
-            dataType: 'json',
-            success: (res) => {
-                for (var i = 0; i < res[0].length; i++) {
-                    document.getElementById(res[0][i]).style.display = 'block'
-                }
-                for (var i = 0; i < res[1].length; i++) {
-                    document.getElementById(res[1][i]).style.display = 'none'
-                }
-            },
-            error: () => {
-                alert('Error')
-            }
-        })
-    })
-    $('#btn-Taschen').click(() => {
-        var data = [7]
-        $.ajax({
-            url: '/category',
-            type: 'POST',
-            contentType: 'application/json',
-            data: JSON.stringify(data),
-            dataType: 'json',
-            success: (res) => {
-                for (var i = 0; i < res[0].length; i++) {
-                    document.getElementById(res[0][i]).style.display = 'block'
-                }
-                for (var i = 0; i < res[1].length; i++) {
-                    document.getElementById(res[1][i]).style.display = 'none'
-                }
-            },
-            error: () => {
-                console.log('Error')
-            }
-        })
-    })
-    $('#btn-1').click(() => {
-        var data = [1]
-        $.ajax({
-            url: '/color',
-            type: 'POST',
-            contentType: 'application/json',
-            data: JSON.stringify(data),
-            dataType: 'json',
-            success: (res) => {
-                for (var i = 0; i < res[0].length; i++) {
-                    document.getElementById(res[0][i]).style.display = 'block'
-                }
-                for (var i = 0; i < res[1].length; i++) {
-                    document.getElementById(res[1][i]).style.display = 'none'
-                }
-            },
-            error: () => {
-                console.log('Error')
-            }
-        })
-    })
-    $('#btn-2').click(() => {
-        var data = [2]
-        $.ajax({
-            url: '/color',
-            type: 'POST',
-            contentType: 'application/json',
-            data: JSON.stringify(data),
-            dataType: 'json',
-            success: (res) => {
-                for (var i = 0; i < res[0].length; i++) {
-                    document.getElementById(res[0][i]).style.display = 'block'
-                }
-                for (var i = 0; i < res[1].length; i++) {
-                    document.getElementById(res[1][i]).style.display = 'none'
-                }
-            },
-            error: (res) => {
-                console.log('Error')
-            }
-        })
-    })
-    $('#btn-3').click(() => {
-        var data = [3]
-        $.ajax({
-            url: '/color',
-            type: 'POST',
-            contentType: 'application/json',
-            data: JSON.stringify(data),
-            dataType: 'json',
-            success: (res) => {
-                for (var i = 0; i < res[0].length; i++) {
-                    document.getElementById(res[0][i]).style.display = 'block'
-                }
-                for (var i = 0; i < res[1].length; i++) {
-                    document.getElementById(res[1][i]).style.display = 'none'
-                }
-            },
-            error: (res) => {
-                console.log('Error')
-            }
-        })
-    })
-    $('#btn-4').click(() => {
-        var data = [4]
-        $.ajax({
-            url: '/color',
-            type: 'POST',
-            contentType: 'application/json',
-            data: JSON.stringify(data),
-            dataType: 'json',
-            success: (res) => {
-                for (var i = 0; i < res[0].length; i++) {
-                    document.getElementById(res[0][i]).style.display = 'block'
-                }
-                for (var i = 0; i < res[1].length; i++) {
-                    document.getElementById(res[1][i]).style.display = 'none'
-                }
-            },
-            error: (res) => {
-                console.log('Error')
-            }
-        })
-    })
-    $('#btn-5').click(() => {
-        var data = [5]
-        $.ajax({
-            url: '/color',
-            type: 'POST',
-            contentType: 'application/json',
-            data: JSON.stringify(data),
-            dataType: 'json',
-            success: (res) => {
-                for (var i = 0; i < res[0].length; i++) {
-                    document.getElementById(res[0][i]).style.display = 'block'
-                }
-                for (var i = 0; i < res[1].length; i++) {
-                    document.getElementById(res[1][i]).style.display = 'none'
-                }
-            },
-            error: (res) => {
-                console.log('Error')
-            }
-        })
-    })
-    $('#btn-6').click(() => {
-        var data = [6]
-        $.ajax({
-            url: '/color',
-            type: 'POST',
-            contentType: 'application/json',
-            data: JSON.stringify(data),
-            dataType: 'json',
-            success: (res) => {
-                for (var i = 0; i < res[0].length; i++) {
-                    document.getElementById(res[0][i]).style.display = 'block'
-                }
-                for (var i = 0; i < res[1].length; i++) {
-                    document.getElementById(res[1][i]).style.display = 'none'
-                }
-            },
-            error: (res) => {
-                console.log('Error')
-            }
-        })
-    })
-    $('#btn-7').click(() => {
-        var data = [7]
-        $.ajax({
-            url: '/color',
-            type: 'POST',
-            contentType: 'application/json',
-            data: JSON.stringify(data),
-            dataType: 'json',
-            success: (res) => {
-                for (var i = 0; i < res[0].length; i++) {
-                    document.getElementById(res[0][i]).style.display = 'block'
-                }
-                for (var i = 0; i < res[1].length; i++) {
-                    document.getElementById(res[1][i]).style.display = 'none'
-                }
-            },
-            error: (res) => {
-                console.log('Error')
-            }
-        })
-    })
-    $('#btn-8').click(() => {
-        var data = [8]
-        $.ajax({
-            url: '/color',
-            type: 'POST',
-            contentType: 'application/json',
-            data: JSON.stringify(data),
-            dataType: 'json',
-            success: (res) => {
-                for (var i = 0; i < res[0].length; i++) {
-                    document.getElementById(res[0][i]).style.display = 'block'
-                }
-                for (var i = 0; i < res[1].length; i++) {
-                    document.getElementById(res[1][i]).style.display = 'none'
-                }
-            },
-            error: (res) => {
-                console.log('Error')
-            }
-        })
-    })
-})
+}
 
-
+var color = (data) => {
+    var fuckingArray = []
+    fuckingArray.push(data)
+    $.ajax({
+        url: '/color',
+        type: 'POST',
+        contentType: 'application/json',
+        data: JSON.stringify(fuckingArray),
+        dataType: 'json',
+        success: (res) => {
+            for (var i = 0; i < res[0].length; i++) {
+                document.getElementById(res[0][i]).style.display = 'block'
+            }
+            for (var i = 0; i < res[1].length; i++) {
+                document.getElementById(res[1][i]).style.display = 'none'
+            }
+        },
+        error: () => {
+            console.log('Error')
+        }
+    })
+}
