c6a2af6d8c5eec1853738b1a1cbc318d0ebe7f59
Matthias <42836408+AdmiralCanelli@users.noreply.github.com>
2019-02-01 08:06:49 +0100

    d

--- README.md
+++ README.md
@@ -13,5 +13,6 @@ Konfiguration:
 Success!
 
 ToDo:
-- zweite Nachkommastelle anzeigen
+- Checkout
+- Filter
 
--- public/javascripts/checkout.js
+++
@@ -1,6 +0,0 @@
-var midas
-$(document).ready(() => {
-    $('#schoolboy').click(()=>{
-        midas += $('#money').val()
-    })
-})
