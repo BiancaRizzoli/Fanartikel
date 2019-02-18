1d6ecfc09cd0836ea194a74a350753298e962f59
Matthias <42836408+AdmiralCanelli@users.noreply.github.com>
2019-02-18 08:19:42 +0100

    Fix checkout.js

--- public/javascripts/checkout.js
+++ public/javascripts/checkout.js
@@ -10,16 +10,9 @@ var addToCheckout = (ArtID) => {
         dataType: 'json',
         success: (res) => {
             $('#total').text(res.price +' €')
-            $('body').after('<div style="position: fixed;bottom: 5px;left: 10px;" class="alert alert-success alert-dismissible fade show text-center" role="alert">Artikel hinzugefügt.<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button></div>')
-            setTimeout(() => {
-                $(".alert").alert('close')
-            }, 5000);
         },
         error: (res) => {
-            $('body').after('<div style="position: fixed;bottom: 5px;left: 10px;" class="alert alert-danger alert-dismissible fade show text-center" role="alert">Error<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button></div>')
-            setTimeout(() => {
-                $(".alert").alert('close')
-            }, 5000);
+            console.log('Error')
         }
     })
 
